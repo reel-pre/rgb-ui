e95abd959b9f62120d6fc4d2ea82eed781dd00c4
reel-pre <hemantb.live@gmail.com>
2021-10-07 21:45:10 +0530

    logo and example changed

--- example/src/App.js
+++ example/src/App.js
@@ -4,6 +4,7 @@ import 'rgb-ui/dist/index.css'
 
 const App = () => {
   return (<>
+<RGBbutton label="RGB-UI" type="scratch"/>
 <p className="classifier">{"<RGBbutton label='Click Me'/>"}</p>
 <RGBbutton label="Click Me" />
 <p className="classifier">{"<RGBbutton label='Click Me' type='scratch'/>"}</p>
